934a290a5be077270a1f63fbc7e51ac77afd53ee
Radomir Vladislavić <vladislavic.rl@gmail.com>
2022-06-13 18:01:49 -0400

    auth

--- frontend/src/Pages/Home.js
+++ frontend/src/Pages/Home.js
@@ -1,38 +1,56 @@
-import React, {useState} from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Button from '../components/Button';
-import '../Styles/home.css';
-
-
-
+import React, { useState } from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Button from "../components/Button";
+import { login } from "../services/login";
+import { useNavigate } from "react-router-dom";
+import "../Styles/home.css";
 
 const Home = () => {
-  const[username, setUsername] = useState('');
-  const[password, setPassword] = useState('');
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const navigate = useNavigate();
 
-  return (
-    <div>
-        <Header />
-        <div className="home-login-container">
-            <div className="home-login">
+    const loginRequest = () => {
+        console.log("click");
+        if(login(username, password)) {
+            navigate("/Main");
+        }
+    };
+
+    return (
+        <div>
+            <Header />
+            <div className="home-login-container">
+                <div className="home-login">
                     <div className="form-container">
                         <label htmlFor="username">Ingrese su Usuario: </label>
-                        <input type="text" id="username" onChange={event => setUsername(event.target.value)}/>        
+                        <input
+                            type="text"
+                            id="username"
+                            onChange={(event) =>
+                                setUsername(event.target.value)
+                            }
+                        />
                     </div>
                     <div className="form-container">
                         <label htmlFor="password">Ingrese su Password: </label>
-                        <input type="password" id="password" onChange={event => setPassword(event.target.value)}/>  
+                        <input
+                            type="password"
+                            id="password"
+                            onChange={(event) =>
+                                setPassword(event.target.value)
+                            }
+                        />
                     </div>
                     <div className="form-container-button">
-                        <Button text='Verificar'/> 
-
+                        <Button text="Verificar" onClick={loginRequest} />
                     </div>
+                </div>
             </div>
+            <Footer />
         </div>
-        <Footer />
-    </div>
-  )
-}
+    );
+};
 
 export default Home;
